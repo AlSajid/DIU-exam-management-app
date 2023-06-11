c12df31ddbf6352cb7dc42086228564e2e41fb7e
Al Sajid <alsajiduzzaman@outlook.com>
2023-06-11 16:28:27 +0600

    Create postData.ts

---
+++ db/utils/postData.ts
@@ -0,0 +1,13 @@
+import connect from "@/db/connect";
+import {NextResponse} from "next/server";
+import errorHandler from "../../utils/errorHandler";
+
+export default async function postData(model: any, data: object, successMessage: string) {
+	try {
+		await connect();
+		await model.create(data);
+		return NextResponse.json({message: successMessage});
+	} catch (error: any) {
+		return errorHandler(error);
+	}
+}

cdde8346ad8910b16ded5a3f26aafeeef5d3e214
Al Sajid <alsajiduzzaman@outlook.com>
2023-06-11 16:28:34 +0600

    Create deleteData.ts

---
+++ db/utils/deleteData.ts
@@ -0,0 +1,16 @@
+import {NextResponse} from "next/server";
+import connect from "../connect";
+import errorHandler from "@/utils/errorHandler";
+
+export default async function deleteData(model: any, id: string, successMessage: string) {
+	try {
+		await connect();
+		const result = await model.deleteOne({_id: id});
+
+		if (result.acknowledged === true && result.deletedCount === 1) {
+			return NextResponse.json({message: successMessage});
+		}
+	} catch (error: any) {
+		return errorHandler(error);
+	}
+}

ed45039319d14a70e861c20500632f263890c59d
Al Sajid <alsajiduzzaman@outlook.com>
2023-06-11 14:02:38 +0600

    Create ViewClassrooms.tsx

---
+++ app/classrooms/ViewClassrooms.tsx
@@ -0,0 +1,35 @@
+import Loader from "@/components/Loader";
+import Table from "@/components/Table";
+import {AllContexts} from "@/contexts/ContextProvider";
+import {useContext} from "react";
+
+const ViewClassrooms = () => {
+	const {sections} = useContext(AllContexts);
+	const handleDelete = async (id: string) => {
+		console.log(id);
+	};
+
+	return (
+		<>
+			{sections === undefined ? (
+				<Loader msg="Fetching data" />
+			) : (
+				<Table
+					name="Sections"
+					type="sections"
+					data={sections}
+					heads={["Semester", "Course", "Teacher", "Section", "Students"]}
+					fields={["Semester", "Course", "Teacher", "Section", "Students"]}
+					actions={[
+						{
+							name: "Delete",
+							onClick: handleDelete
+						}
+					]}
+				/>
+			)}
+		</>
+	);
+};
+
+export default ViewClassrooms;

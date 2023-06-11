6831f9fa533ce46846d251365d4eb4f9a99cf8b2
Al Sajid <alsajiduzzaman@outlook.com>
2023-06-11 14:03:54 +0600

    Create Input.tsx

---
+++ components/Form/Input.tsx
@@ -0,0 +1,27 @@
+export default function Input({input}: any) {
+	return (
+		<tr>
+			<td>{input.label}</td>
+			<td className="w-4/5">
+				{input.type === "select" ? (
+					<select ref={input.ref}>
+						<option value="">...</option>
+						{input.options.map((item: any, index: number) => (
+							<option
+								key={index}
+								value={item.value}>
+								{item.name}
+							</option>
+						))}
+					</select>
+				) : (
+					<input
+						type={input.type}
+						ref={input.ref}
+						onChange={input.onChange}
+					/>
+				)}
+			</td>
+		</tr>
+	);
+}

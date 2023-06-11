4dfdf569b08ccd492c3aa3ab8869350273cb3c6a
Al Sajid <alsajiduzzaman@outlook.com>
2023-06-11 14:00:43 +0600

    context provider added

---
+++ contexts/ContextProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+import {createContext} from "react";
+import useAuth from "./hooks/useAuth";
+import useCourses from "./hooks/useCourses";
+import useSections from "./hooks/useSections";
+import useTeachers from "./hooks/useTeachers";
+
+export const AllContexts = createContext(null);
+
+export default function ContextProvider({children}: {children: React.ReactNode}) {
+	// const authContext = useAuth();
+	const teachersContext = useTeachers();
+	const coursesContext = useCourses();
+	const sectionsContext = useSections();
+
+	const allContexts: any = {
+		// ...authContext,
+		...teachersContext,
+		...coursesContext,
+		...sectionsContext
+	};
+
+	return <AllContexts.Provider value={allContexts}>{children}</AllContexts.Provider>;
+}

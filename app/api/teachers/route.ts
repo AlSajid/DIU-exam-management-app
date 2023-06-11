36ee623cb40caef15c8c757e1a3bf70e102f350d
Al Sajid <alsajiduzzaman@outlook.com>
2023-06-11 16:28:42 +0600

    Update route.ts

--- app/api/teachers/route.ts
+++ app/api/teachers/route.ts
@@ -1,46 +1,20 @@
-import connect from "@/db/connect";
 import Teacher from "@/db/models/teachers";
-import errorHandler from "@/utils/errorHandler";
-import {NextResponse} from "next/server";
+import deleteData from "@/db/utils/deleteData";
+import getData from "@/db/utils/getData";
+import postData from "@/db/utils/postData";
 
 export const POST = async (request: Request) => {
 	const body = await request.json();
-	console.log(body);
-
-	try {
-		await connect();
-		await Teacher.create(body);
-		return NextResponse.json({message: "Teacher Added Successfully"});
-	} catch (error: any) {
-		const message = errorHandler(error);
-		return NextResponse.json({message}, {status: 500});
-	}
+	return await postData(Teacher, body, "Teacher Added Successfully");
 };
 
 export const GET = async () => {
-	try {
-		await connect();
-		const teachers = await Teacher.find();
-		return NextResponse.json(teachers);
-	} catch (error:any) {
-		const message = errorHandler(error);
-		return NextResponse.json({message}, {status: 500});
-	}
+	return await getData(Teacher);
 };
 
 export const DELETE = async (request: Request) => {
 	const {searchParams} = new URL(request.url);
-	const id = searchParams.get("id");
+	const id = searchParams.get("id") || "";
 
-	try {
-		await connect();
-		const result = await Teacher.deleteOne({_id: id});
-
-		if (result.acknowledged === true && result.deletedCount === 1) {
-			return NextResponse.json({message: "Teacher Deleted Successfully"});
-		}
-	} catch (error: any) {
-		const message = errorHandler(error);
-		return NextResponse.json({message}, {status: 500});
-	}
+	return await deleteData(Teacher, id, "Teacher Deleted Successfully");
 };

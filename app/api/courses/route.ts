75edb9600dd182de49a3c834d7d5cc44719d8aaf
Al Sajid <alsajiduzzaman@outlook.com>
2023-06-13 23:18:50 +0600

    PUT method added

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -2,6 +2,7 @@ import Course from "@/db/models/courses";
 import deleteData from "@/db/utils/deleteData";
 import getData from "@/db/utils/getData";
 import postData from "@/db/utils/postData";
+import putData from "@/db/utils/putData";
 
 export const POST = async (request: Request) => {
 	const body = await request.json();
@@ -9,7 +10,15 @@ export const POST = async (request: Request) => {
 };
 
 export const GET = async () => {
-	return await getData(Course);
+	return await getData(Course, null);
+};
+
+export const PUT = async (request: Request) => {
+	const {searchParams} = new URL(request.url);
+	const id = searchParams.get("id") || "";
+
+	const body = await request.json();
+	return await putData(Course, {_id: id}, body, "Course Updated Successfully");
 };
 
 export const DELETE = async (request: Request) => {

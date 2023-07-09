70c3f9294a3ebb7b6e1fb408912c542976a8d12d
Al Sajid <alsajiduzzaman@outlook.com>
2023-07-09 22:05:23 +0600

    Update Loader.tsx

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,7 +1,7 @@
 export default function Loader({msg}: {msg: string}) {
 	return (
 		<div className="my-7 flex flex-col">
-			<div className="w-12 h-12 border-t-emerald-700 border-t-4 rounded-full mx-auto animate-spin my-3"></div>
+			<div className="w-12 h-12 border-t-emerald-700 border-t-4 border-dotted rounded-full mx-auto animate-spin my-3"></div>
 			<h1 className="text-slate-500 text-center">{msg}...</h1>
 		</div>
 	);

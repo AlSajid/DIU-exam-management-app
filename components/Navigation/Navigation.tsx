cc40a2ed85265a6ea69f109fc4ab682cd6639528
Al Sajid <alsajiduzzaman@outlook.com>
2023-07-09 22:04:37 +0600

    home added

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,29 +1,39 @@
-'use client';
-import Link from 'next/link';
-import { options } from './options';
-import { usePathname } from 'next/navigation';
+"use client";
+import Link from "next/link";
+import {options} from "./options";
+import {usePathname} from "next/navigation";
+import {LuLayoutDashboard} from "react-icons/lu";
 
 export default function Navigation() {
-  const pathname = usePathname();
+   const pathname = usePathname();
 
-  return (
-    <nav className='h-screen w-64 bg-emerald-950 py-3 flex items-center '>
-      <ul className='w-full'>
-        {options.map((option, i) => (
-          <li
-            key={option.name}
-            className={
-              pathname === '/' + option.name.toLowerCase() ? 'bg-emerald-100 text-emerald-950 font-bold' : ''
-            }>
-            <Link
-              href={option.name.toLowerCase()}
-              className='flex items-center p-3'>
-              <span>{option.icon}</span>
-              <span className='ml-3'>{option.name}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+   return (
+      <nav className="h-screen w-64 bg-emerald-950 py-3 flex items-center ">
+         <ul className="w-full">
+            <li className={pathname === "/" ? "bg-emerald-100 text-emerald-950 font-bold" : ""}>
+               <Link href="/" className="flex items-center p-3">
+                  <span>
+                     <LuLayoutDashboard />
+                  </span>
+                  <span className="ml-3">Home</span>
+               </Link>
+            </li>
+
+            {options.map((option, i) => (
+               <li
+                  key={option.name}
+                  className={
+                     pathname === "/" + option.name.toLowerCase()
+                        ? "bg-emerald-100 text-emerald-950 font-bold"
+                        : ""
+                  }>
+                  <Link href={option.name.toLowerCase()} className="flex items-center p-3">
+                     <span>{option.icon}</span>
+                     <span className="ml-3">{option.name}</span>
+                  </Link>
+               </li>
+            ))}
+         </ul>
+      </nav>
+   );
 }

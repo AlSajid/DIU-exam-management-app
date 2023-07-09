5af463aa9ee9f8efefadc5dc50eda640446eef52
Al Sajid <alsajiduzzaman@outlook.com>
2023-07-09 22:06:30 +0600

    Update Dropdown.tsx

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,46 +1,42 @@
-import { useState, useRef, useEffect } from "react";
+import {useState, useRef, useEffect} from "react";
 
-export default function Dropdown({ data, setValue, value, name }: any) {
-  const inputRef: any = useRef();
-  const [filteredResults, setFilteredResults] = useState([]);
+export default function Dropdown({data, setValue, initValue, name}: {data: any; setValue: Function; initValue: string | undefined; name: string}) {
+   const inputRef: any = useRef();
+   const [filteredResults, setFilteredResults] = useState([]);
 
-  useEffect(() => {
-    if (value === "") return;
+   useEffect(() => {
+      inputRef.current.value = initValue;
+   }, [initValue]);
 
-    inputRef.current.value = value;
-  }, [value]);
+   const handleInput = () => {
+      setValue("");
+      if (inputRef.current.value === "") {
+         setFilteredResults([]);
+      } else {
+         const filtered = data.filter((item: any) => item[name].toLowerCase().includes(inputRef.current.value.toLowerCase()));
+         setFilteredResults(filtered);
+      }
+   };
 
-  const handleInput = () => {
-    setValue("");
-    if (inputRef.current.value === "") {
+   const handleSelect = (item: any) => {
+      setValue(item._id);
+      inputRef.current.value = item[name];
       setFilteredResults([]);
-    } else {
-      const filtered = data.filter((item: any) => item[name].toLowerCase().includes(inputRef.current.value.toLowerCase()));
-      setFilteredResults(filtered);
-    }
-  };
+   };
 
-  const handleSelect = (item: any) => {
-    setValue(item._id);
-    inputRef.current.value = item[name];
-    setFilteredResults([]);
-  };
+   return (
+      <>
+         <input ref={inputRef} onChange={handleInput} />
 
-  return (
-    <>
-      <input ref={inputRef} onChange={handleInput} />
-
-      {filteredResults.length > 0 && (
-        <ul
-          onBlur={() => setFilteredResults([])}
-          className="absolute min-h-fit max-h-36 overflow-auto border shadow w-96 bg-white p-1 border-emerald-700">
-          {filteredResults.map((item: any, index: number) => (
-            <li className="p-3 text-sm bg-slate-100 my-1 cursor-pointer" key={index} onClick={() => handleSelect(item)}>
-              {item[name]}
-            </li>
-          ))}
-        </ul>
-      )}
-    </>
-  );
+         {filteredResults.length > 0 && (
+            <ul onBlur={() => setFilteredResults([])} className="absolute min-h-fit max-h-36 overflow-auto border shadow w-96 bg-white p-1 border-emerald-700">
+               {filteredResults.map((item: any, index: number) => (
+                  <li className="p-3 text-sm bg-slate-100 my-1 cursor-pointer" key={index} onClick={() => handleSelect(item)}>
+                     {item[name]}
+                  </li>
+               ))}
+            </ul>
+         )}
+      </>
+   );
 }

3c059e75a8f7dd82cfd1ded53a52d2dbeb57af04
Al Sajid <alsajiduzzaman@outlook.com>
2023-07-09 22:03:58 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,36 +5,35 @@ import ContextProvider from "@/contexts/ContextProvider";
 import {Toaster} from "react-hot-toast";
 
 const font = Open_Sans({
-	weight: ["300", "400", "500", "600", "700", "800"],
-	subsets: ["latin"],
-	display: "swap"
+   weight: ["300", "400", "500", "600", "700", "800"],
+   subsets: ["latin"],
+   display: "swap"
 });
 
 export const metadata = {
-	title: "DIU Exam Management System"
+   title: "DIU Exam Management System"
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-	return (
-		<html lang="en">
-			<body className={"bg-slate-100 " + font.className}>
-				<div className="text-xl font-semibold">
-					<Toaster
-						position="top-right"
-						reverseOrder={true}
-					/>
-				</div>
-				<ContextProvider>
-					<div className="flex justify-between h-screen w-full">
-						<Navigation />
-						<div className="grow">{children}</div>
-					</div>
-				</ContextProvider>
-				<div className="md:invisible visible flex items-center text-center text-white justify-center font-serif text-3xl flex-col bg-red-400 h-full w-full fixed top-0 right-0 bottom-0 left-0">
-					<h1 className="text-9xl">Sorry!</h1> <br />
-					<h2>this site is only accessible from Tablet/PC</h2>
-				</div>
-			</body>
-		</html>
-	);
+   return (
+      <html lang="en">
+         <body className={"bg-slate-100 " + font.className}>
+            <div className="text-xl font-semibold">
+               <Toaster position="top-right" reverseOrder={true} />
+            </div>
+
+            <ContextProvider>
+               <div className="flex justify-between h-screen w-full">
+                  <Navigation />
+                  <div className="grow">{children}</div>
+               </div>
+            </ContextProvider>
+
+            <div className="md:invisible visible flex items-center text-center text-white justify-center font-serif text-3xl flex-col bg-red-400 h-full w-full fixed top-0 right-0 bottom-0 left-0">
+               <h1 className="text-9xl">Sorry!</h1> <br />
+               <h2>this site is only accessible from Tablet/PC</h2>
+            </div>
+         </body>
+      </html>
+   );
 }

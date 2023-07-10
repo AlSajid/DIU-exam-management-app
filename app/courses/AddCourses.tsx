bd0802d294190e576bfe058a0e7e1744a7f734aa
Al Sajid <alsajiduzzaman@outlook.com>
2023-07-10 12:10:17 +0600

    Batch feature added

--- app/courses/AddCourses.tsx
+++ app/courses/AddCourses.tsx
@@ -4,83 +4,81 @@ import Form from "@/components/Form/Form";
 import Loader from "@/components/Loader";
 import {useRef, useState, useContext} from "react";
 import {batch} from "@/public/batch";
-import postReqHandler from "@/utils/postReqHandler";
-import {toast} from "react-hot-toast";
+import postReqHandler from "@/utils/ReqHandler/postReqHandler";
 import {AllContexts} from "@/contexts/ContextProvider";
+import validateForm from "@/utils/validateForm";
+import resetForm from "@/utils/resetForm";
 
 export default function AddCourses() {
-	const [loading, setLoading] = useState(false);
-	const {getCourses}: any = useContext(AllContexts);
+   const [loading, setLoading] = useState(false);
+   const {getCourses}: any = useContext(AllContexts);
 
-	const codeRef: any = useRef();
-	const titleRef: any = useRef();
-	const semesterRef: any = useRef();
-	const shiftRef: any = useRef();
+   const codeRef: any = useRef();
+   const titleRef: any = useRef();
+   const semesterRef: any = useRef();
+   const batchRef: any = useRef();
+   const shiftRef: any = useRef();
 
-	const handleAddCourse = async () => {
-		if (codeRef?.current?.value === "") {
-			toast.error("Course Code is required");
-			codeRef?.current?.focus();
-			return;
-		}
+   const handleAddCourse = async () => {
+      const refs = [
+         {input: codeRef, label: "Course Code"},
+         {input: titleRef, label: "Course Title"},
+         {input: semesterRef, label: "Semester"},
+         {input: batchRef, label: "Batch"},
+         {input: shiftRef, label: "Shift"}
+      ];
 
-		if (titleRef?.current?.value === "") {
-			toast.error("Course Title is required");
-			titleRef?.current?.focus();
-			return;
-		}
+      if (validateForm(refs)) return;
 
-		if (semesterRef?.current?.value === "") {
-			toast.error("Semester is required");
-			semesterRef?.current?.focus();
-			return;
-		}
+      const data = {
+         code: codeRef.current.value,
+         title: titleRef.current.value,
+         semester: semesterRef.current.value,
+         batch: batchRef.current.value,
+         shift: shiftRef.current.value
+      };
 
-		if (shiftRef?.current?.value === "") {
-			toast.error("Shift is required");
-			shiftRef?.current?.focus();
-			return;
-		}
+      setLoading(true);
+      const response = await postReqHandler(`/api/courses/`, data);
+      if (response === 200) {
+         getCourses();
+         resetForm([codeRef, titleRef, semesterRef, batchRef, shiftRef]);
+      }
+      setLoading(false);
+   };
 
-		const data = {
-			code: codeRef.current.value,
-			title: titleRef.current.value,
-			semester: semesterRef.current.value,
-			shift: shiftRef.current.value
-		};
+   return (
+      <div className="my-7">
+         <Form
+            input={[
+               {
+                  label: "Course Code",
+                  ref: codeRef,
+                  type: "text",
+                  onChange: (e: any) => (e.target.value = e.target.value.toUpperCase())
+               },
+               {label: "Course Title", ref: titleRef, type: "text"},
+               {label: "Semester", ref: semesterRef, type: "select", options: batch},
+               {label: "Batch", ref: batchRef, type: "number"},
+               {
+                  label: "Shift",
+                  ref: shiftRef,
+                  type: "select",
+                  options: [
+                     {name: "Morning", value: "Morning"},
+                     {name: "Evening", value: "Evening"}
+                  ]
+               }
+            ]}
+         />
 
-		setLoading(true);
-		const response = await postReqHandler(`/api/courses/`, data);
-		if (response === 200) {
-			getCourses();
-			codeRef.current.value = "";
-			titleRef.current.value = "";
-			semesterRef.current.value = "";
-			shiftRef.current.value = "";
-		}
-		setLoading(false);
-	};
-
-	return (
-		<div className="my-7">
-			<Form
-				input={[
-					{label: "Course Code", ref: codeRef, type: "text", onChange: (e: any) => (e.target.value = e.target.value.toUpperCase())},
-					{label: "Course Title", ref: titleRef, type: "text"},
-					{label: "Semester", ref: semesterRef, type: "select", options: batch},
-					{
-						label: "Shift",
-						ref: shiftRef,
-						type: "select",
-						options: [
-							{name: "Morning", value: "Morning"},
-							{name: "Evening", value: "Evening"}
-						]
-					}
-				]}
-			/>
-
-			<div>{loading ? <Loader msg="Sending" /> : <Button action={handleAddCourse}>Add Course</Button>}</div>
-		</div>
-	);
+         <div>
+            {loading ? (
+               <Loader msg="Sending" />
+            ) : (
+               <Button action={handleAddCourse}>Add Course</Button>
+            )}
+         </div>
+      </div>
+   );
 }
--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -5,95 +5,91 @@ import Form from "@/components/Form/Form";
 import Loader from "@/components/Loader";
 import {AllContexts} from "@/contexts/ContextProvider";
 import {batch} from "@/public/batch";
-import putReqHandler from "@/utils/putReqHandler";
+import putReqHandler from "@/utils/ReqHandler/putReqHandler";
+import validateForm from "@/utils/validateForm";
 import {useRouter} from "next/navigation";
 import {useState, useRef, useContext, useEffect} from "react";
 import {toast} from "react-hot-toast";
 
 export default function Page({params}: any) {
-	const {courses, getCourses}: any = useContext(AllContexts);
-	const router = useRouter();
+   const {courses, getCourses}: any = useContext(AllContexts);
+   const router = useRouter();
 
-	const [loading, setLoading] = useState(false);
+   const [loading, setLoading] = useState(false);
 
-	const codeRef: any = useRef();
-	const titleRef: any = useRef();
-	const semesterRef: any = useRef();
-	const shiftRef: any = useRef();
+   const codeRef: any = useRef();
+   const titleRef: any = useRef();
+   const semesterRef: any = useRef();
+   const batchRef: any = useRef();
+   const shiftRef: any = useRef();
 
-	useEffect(() => {
-		if (!courses) return;
-		const info = courses.find((item: any) => item._id === params.id);
+   useEffect(() => {
+      if (!courses) return;
+      const info = courses.find((item: any) => item._id === params.id);
 
-		codeRef.current.value = info.code;
-		titleRef.current.value = info.title;
-		semesterRef.current.value = info.semester;
-		shiftRef.current.value = info.shift;
-	}, [courses, params.id]);
+      codeRef.current.value = info.code;
+      titleRef.current.value = info.title;
+      semesterRef.current.value = info.semester;
+      batchRef.current.value = info.batch;
+      shiftRef.current.value = info.shift;
+   }, [courses, params.id]);
 
-	const handleUpdate = async () => {
-		if (codeRef?.current?.value === "") {
-			toast.error("Course Code is required");
-			codeRef?.current?.focus();
-			return;
-		}
+   const handleUpdate = async () => {
+      const refs = [
+         {input: codeRef, label: "Course Code"},
+         {input: titleRef, label: "Course Title"},
+         {input: semesterRef, label: "Semester"},
+         {input: batchRef, label: "Batch"},
+         {input: shiftRef, label: "Shift"}
+      ];
 
-		if (titleRef?.current?.value === "") {
-			toast.error("Course Title is required");
-			titleRef?.current?.focus();
-			return;
-		}
+      if (validateForm(refs)) return;
 
-		if (semesterRef?.current?.value === "") {
-			toast.error("Semester is required");
-			semesterRef?.current?.focus();
-			return;
-		}
+      const data = {
+         code: codeRef.current.value,
+         title: titleRef.current.value,
+         semester: semesterRef.current.value,
+         shift: shiftRef.current.value
+      };
 
-		if (shiftRef?.current?.value === "") {
-			toast.error("Shift is required");
-			shiftRef?.current?.focus();
-			return;
-		}
+      setLoading(true);
+      const response = await putReqHandler(`/api/courses?id=${params.id}`, data);
+      setLoading(false);
 
-		const data = {
-			code: codeRef.current.value,
-			title: titleRef.current.value,
-			semester: semesterRef.current.value,
-			shift: shiftRef.current.value
-		};
+      if (response === 200) {
+         getCourses();
+         router.push(`/courses`);
+      }
+   };
 
-		setLoading(true);
-		const response = await putReqHandler(`/api/courses?id=${params.id}`, data);
-		setLoading(false);
-
-		if (response === 200) {
-			getCourses();
-			router.push(`/courses`);
-		}
-	};
-
-	return (
-		<Board heading="Update Course Information">
-			<div className="my-7">
-				<Form
-					input={[
-						{label: "Course Code", ref: codeRef, type: "text", onChange: (e: any) => (e.target.value = e.target.value.toUpperCase())},
-						{label: "Course Title", ref: titleRef, type: "text"},
-						{label: "Semester", ref: semesterRef, type: "select", options: batch},
-						{
-							label: "Shift",
-							ref: shiftRef,
-							type: "select",
-							options: [
-								{name: "Morning", value: "Morning"},
-								{name: "Evening", value: "Evening"}
-							]
-						}
-					]}
-				/>
-				<div>{loading ? <Loader msg="Updating" /> : <Button action={handleUpdate}>Update</Button>}</div>
-			</div>
-		</Board>
-	);
+   return (
+      <Board heading="Update Course Information">
+         <div className="my-7">
+            <Form
+               input={[
+                  {
+                     label: "Course Code",
+                     ref: codeRef,
+                     type: "text",
+                     onChange: (e: any) => (e.target.value = e.target.value.toUpperCase())
+                  },
+                  {label: "Course Title", ref: titleRef, type: "text"},
+                  {label: "Semester", ref: semesterRef, type: "select", options: batch},
+                  {
+                     label: "Shift",
+                     ref: shiftRef,
+                     type: "select",
+                     options: [
+                        {name: "Morning", value: "Morning"},
+                        {name: "Evening", value: "Evening"}
+                     ]
+                  }
+               ]}
+            />
+            <div>
+               {loading ? <Loader msg="Updating" /> : <Button action={handleUpdate}>Update</Button>}
+            </div>
+         </div>
+      </Board>
+   );
 }

9f872f19bee3ac63ff61aae3c7cd99eb43d6aacd
Al Sajid <alsajiduzzaman@outlook.com>
2023-07-13 20:21:15 +0600

    reactStrictMode: false

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverComponentsExternalPackages: ["mongoose"]
-	}
+   reactStrictMode: false,
+   experimental: {
+      serverComponentsExternalPackages: ["mongoose"]
+   }
 };
 
 module.exports = nextConfig;

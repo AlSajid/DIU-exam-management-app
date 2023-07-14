2157b5f193c2ac5b5791c6c8b68145f9c2249e68
Al Sajid <alsajiduzzaman@outlook.com>
2023-07-14 09:21:31 +0600

    Update Board.tsx

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,19 +1,20 @@
 export default function Board({
-  heading,
-  children,
+   heading,
+   children
 }: {
-  heading: string;
-  children: React.ReactNode;
+   heading: string;
+   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex flex-col gap-3 p-3">
-      {/* header */}
-      <div className="bg-white text-emerald-700 text-3xl font-bold p-3 shadow rounded">
-        <h1>{heading}</h1>
+   return (
+      <div className="flex flex-col gap-3 p-3 h-screen">
+         {/* header */}
+         <div className="bg-white text-emerald-700 text-3xl font-bold p-3 shadow rounded">
+            <h1>{heading}</h1>
+         </div>
+
+         <div className="shadow bg-white rounded h-full overflow-auto">{children}</div>
       </div>
-      <div className="shadow grow bg-white rounded">{children}</div>
-    </div>
-  );
+   );
 }
 
 // {/*

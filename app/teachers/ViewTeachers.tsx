73fef144f3301d5d075d1dfed4e45e407b50b0fc
Al Sajid <alsajiduzzaman@outlook.com>
2023-07-14 09:21:45 +0600

    Update ViewTeachers.tsx

--- app/teachers/ViewTeachers.tsx
+++ app/teachers/ViewTeachers.tsx
@@ -1,15 +1,21 @@
 "use client";
 import Loader from "@/components/Loader";
-import Table from "@/components/Table";
 import {AllContexts} from "@/contexts/ContextProvider";
 import deletionModal from "@/components/DeletionModal";
 import delReqHandler from "@/utils/ReqHandler/delReqHandler";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
+import ToggleSwitch from "@/components/toggleSwitch";
+import {Teacher} from "@/types/teachers";
 
 const ViewTeachers = () => {
    const {teachers, getTeachers}: any = useContext(AllContexts);
+   const [list, setList] = useState<Teacher[]>([]);
    const router = useRouter();
+
+   useEffect(() => {
+      if (teachers) setList(teachers);
+   }, [teachers]);
 
    const handleDelete = (id: string) => {
       async function del() {
@@ -26,33 +32,57 @@ const ViewTeachers = () => {
       router.push(`/teachers/${id}`);
    };
 
+   const handleSearch = (query: string) => {
+      const result = teachers.filter((teacher: Teacher) => {
+         const name = teacher.name.toLowerCase();
+         return name.includes(query.toLowerCase()) || teacher.employeeID == query;
+      });
+      setList(result);
+   };
+
    return (
       <>
          {teachers === undefined ? (
             <Loader msg="Fetching data" />
          ) : (
             <table>
-               <thead className="sticky top-0 shadow">
+               <thead>
                   <tr>
-                     <td>Employee ID</td>
-                     <td>Name</td>
-                     <td>Designation</td>
-                     <td>Phone</td>
-                     <td>Email</td>
-                     <td>Actions</td>
+                     <td className="w-1/12">Employee ID</td>
+                     <td className="w-3/12">Name</td>
+                     <td className="w-2/12">Designation</td>
+                     <td className="w-2/12">Phone</td>
+                     <td className="w-2/12">Email</td>
+                     <td className="w-2/12">
+                        <input
+                           placeholder="Search"
+                           onChange={(e) => handleSearch(e.target.value)}
+                        />
+                     </td>
                   </tr>
                </thead>
                <tbody>
-                  {teachers.map((teacher: any, index: number) => (
-                     <tr key={teacher._id} className="text-center even:bg-slate-100">
+                  {list.map((teacher: any, index: number) => (
+                     <tr key={teacher._id} className=" even:bg-slate-100">
                         <td key={index}>{teacher.employeeID}</td>
-                        <td className="text-left">{teacher.name}</td>
+
+                        <td>
+                           <a href={teacher.profile}>{teacher.name}</a>
+                        </td>
+
                         <td>{teacher.designation}</td>
+
                         <td>
                            <a href={`tel:${teacher.phone}`}>{teacher.phone}</a>
                         </td>
-                        <td>{teacher.email}</td>
-                        <td className="flex justify-around items-center">
+
+                        <td>
+                           <a href={`mailto:${teacher.email}`}>{teacher.email}</a>
+                        </td>
+
+                        <td className="flex items-center justify-evenly relative z-0" colSpan={2}>
+                           <ToggleSwitch state={true} />
+
                            <button
                               className="bg-transparent px-1 font-bold text-black hover:text-red-700"
                               onClick={() => handleEdit(teacher._id)}>

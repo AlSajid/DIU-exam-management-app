047cdddc117eed8d1e8bd7fe7eb107b1974d0cb0
Al Sajid <alsajiduzzaman@outlook.com>
2023-08-01 00:15:45 +0600

    Update getData.ts

--- db/utils/getData.ts
+++ db/utils/getData.ts
@@ -1,10 +1,10 @@
 import connect from "@/db/connect";
-import errorHandler from "../../utils/errorHandler";
+import errorHandler from "@/utils/errorHandler";
 
 export default async function getData(model: any, filter: any) {
    try {
       await connect();
-      return await model.find(filter);
+      return await model.find(filter, );
    } catch (error: any) {
       return errorHandler(error);
    }
